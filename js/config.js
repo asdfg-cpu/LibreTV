1ff13e711de337dda580da967d2836e373846170
asdfg-cpu <3978857475@qq.com>
2025-12-06 23:36:59 +0800

    Update config.js
    add api

--- js/config.js
+++ js/config.js
@@ -45,3 +45,111 @@ const SECURITY_CONFIG = { enableXSSProtection: true, sanitizeUrls: true, maxQuer
 const CUSTOM_API_CONFIG = { separator: ',', maxSources: 5, testTimeout: 5000, namePrefix: 'Custom-', validateUrl: true, cacheResults: true, cacheExpiry: 5184000000, adultPropName: 'isAdult' };
 const HIDE_BUILTIN_ADULT_APIS = false;
 
+// API站点配置（2025.12 精选稳定源 - 基于用户分享 + 社区验证）
+const API_SITES = {
+    // 热门基础源
+    hongniu: {
+        api: 'http://hongniuzy2.com/api.php/provide/vod',
+        name: '紅牛資源',
+        adult: false
+    },
+    liangzi: {
+        api: 'http://cj.lziapi.com/api.php/provide/vod',
+        name: '量子資源',
+        adult: false
+    },
+    kaifang: {
+        api: 'http://vod-demo.onrender.com/pubdovod.php',
+        name: '開放電影',
+        adult: false
+    },
+    youzhi: {
+        api: 'http://api.1080zyku.com/inc/api.php/provide/vod',
+        name: '优质资源库',
+        adult: false
+    },
+    sanjiu: {
+        api: 'http://39kan.com/api.php/provide/vod',
+        name: '39影視',
+        adult: false
+    },
+    tiantang: {
+        api: 'http://vipmv.cc/api.php/provide/vod',
+        name: '天堂资源',
+        adult: false
+    },
+    tangren: {
+        api: 'http://tangrenjie.tv/api.php/provide/vod',
+        name: '唐人街',
+        adult: false
+    },
+    kudian: {
+        api: 'http://api.kuapi.cc/api.php/provide/vod',
+        name: '酷点资源',
+        adult: false
+    },
+    wolong: {
+        api: 'http://collect.wolongzyw.com/api.php/provide/vod',
+        name: '卧龙资源',
+        adult: false
+    },
+    // 扩展源（2025 新热）
+    fantuan: {
+        api: 'https://www.fantuan.tv/api.php/provide/vod',
+        name: '饭团TV',
+        adult: false
+    },
+    qiqidy: {
+        api: 'https://www.qiqidys.com/api.php/provide/vod',
+        name: '七七资源',
+        adult: false
+    },
+    yinghua: {
+        api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
+        name: '樱花资源',
+        adult: false
+    },
+    baiduzy: {
+        api: 'https://api.apibdzy.com/api.php/provide/vod',
+        name: '百度资源',
+        adult: false
+    },
+    heimuer: {
+        api: 'https://www.heimuer.tv/api.php/provide/vod',
+        name: '黑木耳',
+        adult: false
+    },
+    ffzy: {
+        api: 'http://cj.ffzyapi.com/api.php/provide/vod',
+        name: '非凡',
+        adult: false
+    },
+    // 更多（可选加）
+    slapibf: {
+        api: 'http://slapibf.com/api.php/provide/vod',
+        name: '森林资源',
+        adult: false
+    },
+    ykapi: {
+        api: 'http://api.ykapi.net/api.php/provide/vod',
+        name: '影库资源网',
+        adult: false
+    },
+    kczy: {
+        api: 'http://caiji.kczyapi.com/api.php/provide/vod',
+        name: '快车资源',
+        adult: false
+    },
+    sdzy: {
+        api: 'http://sdzyapi.com/api.php/provide/vod',
+        name: '閃電資源',
+        adult: false
+    },
+    aosika: {
+        api: 'http://aosikazy.com/api.php/provide/vod',
+        name: '奥斯卡资源网',
+        adult: false
+    }
+    // 注意：失效源如 lehootv.com 已排除；重复源已合并
+};
+
